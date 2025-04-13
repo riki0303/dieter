3a6180110a2420f8584539ca93e09a4da04a4a7a
小久保力輝 <kokub0303@gmail.com>
2025-04-13 18:08:41 +0900

    add: migrate

---
+++ db/migrate/20250413085347_create_fitlogs.rb
@@ -0,0 +1,15 @@
+class CreateFitlogs < ActiveRecord::Migration[8.0]
+  def change
+    create_table :fitlogs do |t|
+      t.references :user, null: false, foreign_key: true
+      t.decimal :weight, null:false, precision: 4, scale: 1, comment: '体重'  # 最大値 999.9 まで扱える設定例
+      t.decimal :body_fat, precision: 4, scale: 1, comment: '体脂肪率'  # 最大値 99.9 まで扱える設定例
+      t.decimal :muscle, precision: 4, scale: 1, comment: '筋肉量'  # 最大値 999.9 まで扱える設定例
+      t.decimal :bmr, precision: 4, scale: 1, comment: '基礎代謝量'  # 最大値 9999.9 まで扱える設定例
+      t.decimal :body_age, precision: 4, scale: 1, comment: '体年齢'  # 最大値 99.9 まで扱える設定例
+      t.text :memo, comment: 'メモ'
+
+      t.timestamps
+    end
+  end
+end
