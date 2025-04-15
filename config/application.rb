62062ec5eb2618516ed1088df2082a8a7b7d55be
小久保力輝 <kokub0303@gmail.com>
2025-04-15 10:24:19 +0900

    add: 日本語化

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Dieter
     # config.time_zone = "Central Time (US & Canada)"
     config.time_zone = 'Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
