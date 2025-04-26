c1ce0081b99b5c902eec012c3c39bd63695d6631
小久保力輝 <kokub0303@gmail.com>
2025-04-26 17:23:33 +0900

    add: ユーザー退会時は論理削除するように

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Discard::Model
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +8,21 @@ class User < ApplicationRecord
 
   has_many :fitlogs, dependent: :destroy
   has_one :profile, dependent: :destroy
+
+  # deviceのメソッドをオーバーライド
+  def active_for_authentication?
+    super && !discarded?
+  end
+
+  def withdraw
+    ActiveRecord::Base.transaction do
+
+      # 関連レコードを削除
+      fitlogs.destroy_all
+      profile&.destroy
+
+      # ユーザーを論理削除
+      discard
+    end
+  end
 end
