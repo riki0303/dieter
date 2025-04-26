abc0dc05cad5c542016879320589c14e606c811f
小久保力輝 <kokub0303@gmail.com>
2025-04-26 17:31:15 +0900

    add:　トランザクション内で例外が発生した場合のエラーハンドリング追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: %i[ withdraw ]
 
@@ -7,5 +6,10 @@ class UsersController < ApplicationController
 
     sign_out current_user
     redirect_to new_user_session_path, notice: '退会処理が完了しました。ご利用ありがとうございました。'
+
+  rescue => e
+    # エラーハンドリング
+    logger.error "user_id: #{current_user.id} の退会処理に失敗しました: #{e.message}"
+    redirect_to edit_user_registration_path, alert: "退会処理に失敗しました。"
   end
 end
