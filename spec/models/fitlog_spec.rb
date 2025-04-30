86f867cf1b5f746fb40c18795ed12e255ee870cd
小久保力輝 <kokub0303@gmail.com>
2025-04-30 20:46:36 +0900

    コメントアウト

--- spec/models/fitlog_spec.rb
+++ spec/models/fitlog_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
-
-RSpec.describe Fitlog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# require 'rails_helper'
+#
+# RSpec.describe Fitlog, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
